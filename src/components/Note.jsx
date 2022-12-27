5f0e095206f548d69765886e11c4c31367258239
Javez <mr.javez@ukr.net>
2022-12-28 00:14:43 +0200

    First test realisation of react components add: -index.css -App.jsx add and modify: -Footer.jsx -Header.jsx -Note.jsx

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+function Note() {
+    return <div className="note">
+        <h1>Test</h1>
+        <p>testing</p>
+    </div>;
+}
+
+export default Note;
